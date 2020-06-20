e3ee7a93b78c96575a1b4a77875510d92d07b7fd
Eric Y <eye@usc.edu>
2020-06-20 14:59:07 -0700

    Added all api routes

--- backend/hello.py
+++ backend/hello.py
@@ -1,33 +1,43 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
-
 @app.route('/user', methods=['GET', 'POST'])
-def get_user():
-    pass
+def user():
+    if request.method == "GET":
+        return get_all_users()
+    elif request.method == "POST":
+        create_user(request.json)
 
 
-@app.route('/restaurant/last', methods=['GET', 'PATCH'])
-def last_restaurant():
-    pass
+@app.route('/user/<string:uid>')
+def get_user(uid):
+    return get_user(uid)
 
 
-@app.route('/restaurant/saved', methods=["GET", "POST"])
-def save_restaurant():
-    pass
+@app.route('/restaurant/last/<string:uid>', methods=['GET', 'PATCH'])
+def last_restaurant(uid):
+    if request.method == "GET":
+        return get_last_restaurant(uid)
+    elif request.method == "PATCH":
+        change_last_restaurant(uid, request.json)
 
 
-@app.route('/left')
+@app.route('/restaurant/saved/<string:uid>', methods=["GET", "POST", "DELETE"])
+def saved_restaurant():
+    if request.method == "GET":
+        return get_saved_restaurants(uid)
+    if request.method == "POST":
+        add_saved_restaurant(uid, request.json)
+    elif request.method == "DELETE":
+        delete_save_restaurant(uid, request.json)
+
+@app.route('/left/<string:uid>', methods=["POST"])
 def dislike_restaurant():
-    pass
+    add_diskliked_restaurant(request.json)
 
 
-@app.route('/right')
+@app.route('/right/<string:uid>', methods=["POST"])
 def meh_restaurant():
-    pass
+    add_meh_restaurant(request.json)
