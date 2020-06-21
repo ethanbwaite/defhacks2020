ab67948a0504ff0a0ab1642e02c08d4528c68908
jason.jen <jj@jj-jj.org>
2020-06-21 14:33:16 -0700

    background

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -6,4 +6,4 @@ git clone --single-branch --branch backend_eric git@github.com:warthogs32/defhac
 cp -r defhacks2020/backend/* ~/prod
 cp defhacks2010-firebase-adminsdk-bxhf2-1845282f87.json ~/prod
 sudo service nginx start
-python3.7 ~/prod/UserController.py
+python3.7 ~/prod/UserController.py &
