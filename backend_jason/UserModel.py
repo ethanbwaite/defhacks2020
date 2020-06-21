0f4fb2bfe6dc9a60308fdf596f60b779596e633d
jason.jen <jj@jj-jj.org>
2020-06-20 19:50:27 -0700

    user controller

--- backend_jason/UserModel.py
+++
@@ -1,8 +0,0 @@
-from typing import Dict
-class UserModel:
-    def __init__(self):
-    	self.Phone: str = ""
-    	self.LastViewedRestaurant: str = ""
-    	self.Left: List[str] = []
-    	self.Right: List[str] = []
-    	self.Saved: List[str] = []
