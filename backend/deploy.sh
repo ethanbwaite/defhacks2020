a22b74a07413ef7a466bd2a085f858cd4d98f35b
jason.jen <jj@jj-jj.org>
2020-06-21 14:31:07 -0700

    fix deploy

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,8 +1,9 @@
-sudo service nginx start
+sudo service nginx stop
 rm -rf ~/prod
 rm -rf ~/defhacks2020
 mkdir prod
 git clone --single-branch --branch backend_eric git@github.com:warthogs32/defhacks2020.git
 cp -r defhacks2020/backend/* ~/prod
+cp defhacks2010-firebase-adminsdk-bxhf2-1845282f87.json ~/prod
 sudo service nginx start
 python3.7 ~/prod/UserController.py
