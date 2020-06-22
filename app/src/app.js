18a4e74ca7e0c3a0925ed71ff5c0f1176c9250a9
ethanbwaite <ebwaite@calpoly.edu>
2020-06-21 17:20:34 -0700

    Create app.js

---
+++ app/src/app.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react';
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
+
+import Login from './components/Login.js';
+import CardsPage from './CardsPage.js';
+
+import './App.css';
+
+function App() {
+
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route path="/" component={CardsPage} />
+        </Switch>
+      </Router>
+    </div>
+  );
+
+}
+
+export default App;
